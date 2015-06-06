d057970f0c031fd02fe691b9f090a5019a00e642
FarisB <faris26000@gmail.com>
2015-06-06 16:55:49 +0200

    la naissance du commit

--- iutvalence-projets2-2015-grp1/src/fr/iutvalence/gpr1/java/Application.java
+++ iutvalence-projets2-2015-grp1/src/fr/iutvalence/gpr1/java/Application.java
@@ -9,12 +9,12 @@ import fr.iutvalence.gpr1.java.view.console.IHMConsole;
 
 public class Application {
 
-	public static void main(String[] args) {
-		File listEtudiants = new File("ListeEtudiants.txt");
-		File listProfesseurs = new File("ListeProfesseurs.txt");
-		File listAdministrateurs = new File("ListeAdministrateurs.txt");
-		IHM monIHM = new IHMConsole();
-		Gestionnaire monGestionnaire = new Gestionnaire(monIHM, listEtudiants, listProfesseurs, listAdministrateurs);
-	}
+        public static void main(String[] args) {
+                File listEtudiants = new File("ListeEtudiants.txt");
+                File listProfesseurs = new File("ListeProfesseurs.txt");
+                File listAdministrateurs = new File("ListeAdministrateurs.txt");
+                IHM monIHM = new IHMConsole();
+                Gestionnaire monGestionnaire = new Gestionnaire(monIHM, listEtudiants, listProfesseurs, listAdministrateurs);
+        }
 
 }
