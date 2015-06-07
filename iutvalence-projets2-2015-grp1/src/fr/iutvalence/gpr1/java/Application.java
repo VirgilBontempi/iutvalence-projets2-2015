a07a4c08eb0d24f5aaf9dba6db4029349b49d6c7
FarisB <faris26000@gmail.com>
2015-06-07 18:51:56 +0200

    Ca affiche encore mieux

--- iutvalence-projets2-2015-grp1/src/fr/iutvalence/gpr1/java/Application.java
+++ iutvalence-projets2-2015-grp1/src/fr/iutvalence/gpr1/java/Application.java
@@ -16,19 +16,11 @@ import fr.iutvalence.gpr1.java.view.console.IHMConsole;
 public class Application {
 
         public static void main(String[] args) {
-                File listEtudiants = new File("ListeEtudiants.txt");
-                ListePersonnes a= new ListePersonnes(listEtudiants);
-//                try(PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("ListeEtudiants.txt", true)))) {
-//                	Iterator i1 = a.getListEtudiants().iterator();
-//                	for (int j = 0; j == a.getListEtudiants().size(); j++) {
-//            			System.out.print(a.getListEtudiants().get(j) + ",");
-//              		}
-//                }catch (IOException e) {
-//                    //exception handling left as an exercise for the reader
-//                }
-//                
-                
-                
+
+        File FichierListe = new File("ListeEtudiants.txt");
+        
+        ListePersonnes a= new ListePersonnes(FichierListe);     
+        
                 try {
 					System.out.println(a.getListEtudiants());
 				} catch (IOException e) {
@@ -39,7 +31,7 @@ public class Application {
                 File listProfesseurs = new File("ListeProfesseurs.txt");
                 File listAdministrateurs = new File("ListeAdministrateurs.txt");
                 IHM monIHM = new IHMConsole();
-                Gestionnaire monGestionnaire = new Gestionnaire(monIHM, listEtudiants, listProfesseurs, listAdministrateurs);
+//                Gestionnaire monGestionnaire = new Gestionnaire(monIHM, listEtudiants, listProfesseurs, listAdministrateurs);
         }
 
 }
